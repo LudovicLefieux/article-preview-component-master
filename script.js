aa0f5259a072a1fc716acb39add4af5cd4ec0b0b
LudovicLefieux <lefieux.ludovic@protonmail.com>
2021-09-25 16:25:13 +0200

    Change button's color on click

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
-const shareButton = document.querySelectorAll(".share-button");
+const shareButton = document.querySelectorAll("a.share-button");
 
-function displayShareSection(e) {
+function displayShareSection() {
   const shareSectionDesktop = document.querySelector("#share-section-desktop");
   const shareSectionMobile = document.querySelector("#share-section-mobile");
   let screen = window.matchMedia("(min-width: 900px)");
-  let target = e.target;
+  let buttonArrow = this.children[0].children[0];
   if (screen.matches && shareSectionDesktop.classList.contains("active")) {
-    target.classList.remove("share-button--dark");
+    this.classList.remove("share-button--dark");
+    buttonArrow.setAttribute("fill", "hsl(217, 19%, 35%)");
     shareSectionDesktop.classList.add("pop-out");
     setTimeout(() => {
       shareSectionDesktop.classList.remove("active", "pop-out");
     }, 500);
   } else if (screen.matches) {
     shareSectionDesktop.classList.add("active");
-    target.classList.add("share-button--dark");
+    this.classList.add("share-button--dark");
+    buttonArrow.setAttribute("fill", "hsl(210, 46%, 95%)");
   } else if (shareSectionMobile.classList.contains("active")) {
     shareSectionMobile.classList.add("slide-down");
     setTimeout(() => {
